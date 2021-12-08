71d72a44230f89aa5dcd0fc2a9e1f226865b242d
m-guberina <gubi.guberina@gmail.com>
2021-12-08 11:13:55 +0100

    pls

---
+++ runs.py
@@ -0,0 +1,7 @@
+"""
+here write different runs
+make that multiprocessed (marko's job)
+
+you can call this from main
+but first write a generic main for a single auction-market_clearing-mechanism_allocation iteration
+"""
